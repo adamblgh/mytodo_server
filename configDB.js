d55e4b628b463b1b51ee4769421b1cd2710e85d9
Your Name <you@example.com>
2022-11-08 11:39:30 +0100

    serveredit

--- configDB.js
+++ configDB.js
@@ -1,7 +1,20 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-export const configDB = {
-    host :"localhost",
-    user : "root",
-    password : "",
-    database :"todo"
-};
+/*export const config = 
+{
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'books'
+}*/
+
+export const configDB = 
+{
+    host     : process.env.MYSQL_HOST || 'localhost',
+    user     : process.env.MYSQL_USERNAME || 'root',
+    password : process.env.MYSQL_PASSWORD || '',
+    database : process.env.MYSQL_DATABASE || 'todos'
+}
+
+console.log("Adatbázis: ",configDB.database)
